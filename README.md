ec16976c440279feb65a66c85d88b6095dd84503
divppc <okal.ihor@gmail.com>
2018-10-05 16:27:08 +0300

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+**Install:**
+
+```bash
+npm install
+```
+
+**Run dev**
+
+```bash
+gulp
+```
+
+for css changes, please edit src/scss/styles.scss, compiled and minified output css will appear in the dist/assets/css/style.css
---
+++ dist/assets/js/main.js
@@ -0,0 +1,69 @@
+window.addEventListener("load", function() {
+  const tabLinks = Array.from(document.querySelectorAll(".tabs-list a"));
+  const tabs = Array.from(document.querySelectorAll(".tab"));
+
+  tabLinks.map(el => {
+    el.addEventListener("click", e => {
+      e.preventDefault();
+      clearClasses(tabLinks);
+      e.target.classList.add("active");
+      const clickedTab = e.target.getAttribute("href");
+      clearClasses(tabs);
+      document.querySelector(clickedTab).classList.add("active");
+    });
+  });
+
+  const clearClasses = arr => {
+    arr.map(el => {
+      el.classList.remove("active");
+    });
+  };
+
+  const firstStepInputs = document.querySelectorAll("form .step.first input");
+  const steps = Array.from(document.querySelectorAll("form .step"));
+  const serviceInput = document.querySelector("input.service");
+  const dateInput = document.querySelector("input.date");
+
+  console.log(firstStepInputs);
+
+  for (var i = 0; i < firstStepInputs.length; i++) {
+    firstStepInputs[i].addEventListener("keypress", e => {
+      if (
+        e.keyCode == 13 &&
+        !!serviceInput.value.length &&
+        !!dateInput.value.length
+      ) {
+        clearClasses(steps);
+        document.querySelector(".step.with-submit").classList.add("active");
+      }
+    });
+  }
+
+  var elements = document.getElementsByClassName("form");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(
+      "submit",
+      function(evt) {
+        var http = new XMLHttpRequest(),
+          f = this;
+        SendData = new FormData(f);
+
+        console.log(SendData);
+        evt.preventDefault();
+
+        http.open("POST", "form.php", true);
+        http.onreadystatechange = function() {
+          if (http.readyState == 4 && http.status == 200) {
+            clearClasses(steps);
+            document.querySelector(".step.finish").classList.add("active");
+          }
+        };
+        http.onerror = function() {
+          // error
+        };
+        http.send(SendData);
+      },
+      false
+    );
+  }
+});
